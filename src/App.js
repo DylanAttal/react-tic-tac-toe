61eb6f75d0638dac78c8c618ced7999aee45fa4d
Dylan Attal <attal.dylan@gmail.com>
2018-11-26 16:48:12 -0500

    Creates app. Creates win logic. Styles squares.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,119 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentTurn: 'X',
+      board: ['', '', '', '', '', '', '', '', ''],
+      message: 'Enjoy the Game!',
+      isThereAWinner: 'false'
+    }
+  }
+
+  _click = index => {
+    // Doesn't let user click if the square is already
+    // occupied by an 'X' or 'O'
+    if (this.state.board[index] !== '') {
+      return
+    }
+    // Doesn't let user click if there is already
+    // a winner to the game
+    if (this.state.isThereAWinner === 'true') {
+      return
+    }
+
+    if (this.state.currentTurn === 'X') {
+      this.state.board[index] = 'X'
+      this.setState(
+        {
+          board: this.state.board,
+          currentTurn: 'O'
+        },
+        () => {
+          this.checkForTie()
+          this.checkForWinner('X')
+        }
+      )
+    } else if (this.state.currentTurn === 'O') {
+      this.state.board[index] = 'O'
+      this.setState(
+        {
+          board: this.state.board,
+          currentTurn: 'X'
+        },
+        () => {
+          this.checkForTie()
+          this.checkForWinner('O')
+        }
+      )
+    }
+  }
+
+  checkForTie = () => {
+    if (
+      !this.checkForWinner('X') &&
+      !this.checkForWinner('O') &&
+      this.state.board.every(square => square === 'X' || square === 'O')
+    ) {
+      return this.setState({
+        message: "It's a tie!",
+        isThereAWinner: 'true'
+      })
+    }
+  }
+
+  checkForWinner = letter => {
+    let winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    return winningCombinations.some(combination => {
+      if (
+        this.state.board[combination[0]] !== '' &&
+        this.state.board[combination[1]] !== '' &&
+        this.state.board[combination[2]] !== '' &&
+        this.state.board[combination[0]] === letter &&
+        this.state.board[combination[1]] === letter &&
+        this.state.board[combination[2]] === letter
+      ) {
+        return this.setState({
+          message: `${letter} wins!`,
+          isThereAWinner: 'true'
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Tic Tac Toe</h1>
+        <h2 className={this.state.isThereAWinner}>{this.state.message}</h2>
+        <div className="board">
+          {this.state.board.map((square, index) => {
+            return (
+              <div
+                className="square"
+                onClick={() => this._click(index)}
+                key={index}
+              >
+                {square}
+              </div>
+            )
+          })}
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
